aec3fb80d1a962127ce2f6a4dddfc424517829b3
hypnopompicindex <hypnopompicindex@gmail.com>
2018-06-07 13:58:11 -0400

    Annoucement mode updates

--- presentationmode/admin.py
+++ presentationmode/admin.py
@@ -5,5 +5,5 @@ from adminsortable2.admin import SortableAdminMixin
 
 @admin.register(Presentation)
 class PresentationAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ['title', 'image_overlay',  'active', 'start_date', 'end_date']
+    list_display = ['title', 'image_overlay', 'start_date', 'end_date', 'active']
     list_editable = ('active',)
--- presentationmode/models.py
+++ presentationmode/models.py
@@ -5,10 +5,10 @@ from filebrowser.fields import FileBrowseField
 class Presentation(models.Model):
     title = models.CharField(max_length=255)
     order = models.PositiveIntegerField(default=0, blank=False, null=False)
-    image_overlay = FileBrowseField("Image Overlay", max_length=200, directory="presentation/image_overlay/", extensions=['.jpg', '.jpeg', '.gif', '.png', '.tif', '.tiff'], blank=True)
-    active = models.BooleanField(default=False)
+    image_overlay = FileBrowseField("Image Overlay", max_length=200, directory="presentation/image_overlay/", extensions=['.jpg', '.jpeg', '.gif', '.png', '.tif', '.tiff'])
     start_date = models.DateTimeField(blank=True, null=True)
     end_date = models.DateTimeField(blank=True, null=True)
+    active = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['order']
