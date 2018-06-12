7bbf784cbbe46f987962faa60cdb9249d7f0d324
hypnopompicindex <hypnopompicindex@gmail.com>
2018-06-12 13:17:27 -0400

    kids zone updates

--- presentationmode/admin.py
+++ presentationmode/admin.py
@@ -7,3 +7,5 @@ from adminsortable2.admin import SortableAdminMixin
 class PresentationAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ['title', 'image_overlay', 'start_date', 'end_date', 'active']
     list_editable = ('active',)
+    readonly_fields = ('image_overlay_path',)
+    fields = ('title', 'image_overlay', 'image_overlay_path', 'start_date', 'end_date', 'active')
--- presentationmode/models.py
+++ presentationmode/models.py
@@ -5,9 +5,9 @@ from filebrowser.fields import FileBrowseField
 class Presentation(models.Model):
     title = models.CharField(max_length=255)
     order = models.PositiveIntegerField(default=0, blank=False, null=False)
-    image_overlay = FileBrowseField("Image Overlay", max_length=200, directory="presentation/image_overlay/", extensions=['.jpg', '.jpeg', '.gif', '.png', '.tif', '.tiff'])
-    start_date = models.DateTimeField(blank=True, null=True)
-    end_date = models.DateTimeField(blank=True, null=True)
+    image_overlay = FileBrowseField("Image Overlay", max_length=200, directory="announcement/image_overlay/", extensions=['.jpg', '.jpeg', '.gif', '.png', '.tif', '.tiff'])
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
     active = models.BooleanField(default=False)
 
     class Meta:
@@ -16,3 +16,7 @@ class Presentation(models.Model):
 
     def __str__(self):
         return self.title
+
+    @property
+    def image_overlay_path(self):
+        return self.image_overlay.path
