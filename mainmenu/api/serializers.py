bcbcac6fe93256c4ee3b5284838d22dae525a68a
hypnopompicindex <hypnopompicindex@gmail.com>
2018-06-13 13:56:34 -0400

    added API for main menu intermediary table

--- mainmenu/api/serializers.py
+++ mainmenu/api/serializers.py
@@ -1,4 +1,4 @@
-from mainmenu.models import ContentGroup, ContentCard, ContentGallery
+from mainmenu.models import ContentGroup, ContentCard, ContentGallery, ContentGroupCard, ContentLabel
 from rest_framework import serializers
 
 
@@ -6,6 +6,12 @@ class ContentGallerySerializer(serializers.ModelSerializer):
     class Meta:
         model = ContentGallery
         fields = ('title', 'priority', 'gallery_image')
+
+
+class ContentLabelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContentLabel
+        fields = ('id', 'label')
 
 
 class ContentCardSerializer(serializers.ModelSerializer):
@@ -16,9 +22,21 @@ class ContentCardSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'creation_date', 'active', 'date_override', 'content_type', 'invert_content_view', 'text_header', 'text', 'video', 'text_position', 'gradient_overlay', 'galleries')
 
 
+class ContentGroupCardSerializer(serializers.ModelSerializer):
+    card_model = ContentCardSerializer(read_only=True,)
+    label = ContentLabelSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = ContentGroupCard
+        fields = ('id', 'content_card', 'priority', 'label', 'card_model')
+        depth = 2
+
+
 class ContentGroupSerializer(serializers.ModelSerializer):
     card = ContentCardSerializer(read_only=True, many=True)
+    content_group = ContentGroupCardSerializer(source='contentgroupcard_set', many=True)
 
     class Meta:
         model = ContentGroup
-        fields = ('id', 'title', 'active', 'secret', 'lft', 'rght', 'tree_id', 'level', 'parent', 'card')
+        fields = ('id', 'title', 'active', 'secret', 'lft', 'rght', 'tree_id', 'level', 'parent', 'card', 'content_group')
+
