388ad0424ffecc832b009265d16af66ceb18bde6
hypnopompicindex <hypnopompicindex@gmail.com>
2018-06-20 14:18:26 -0400

    entice mode video thumbnails

--- enticemode/models.py
+++ enticemode/models.py
@@ -31,28 +31,28 @@ class Sequence(models.Model):
         if self.intro_video == '':
             return ''
         else:
-            return mark_safe('<a href="/media/uploads/entice_mode/video_list/intro_video_thumbnails/%s_intro_thumbnail.png" target="_blank"><img src="/media/uploads/entice_mode/video_list/intro_video_thumbnails/%s_intro_thumbnail.png" width="200" /></a>' % (self.intro_video.filename_root, self.intro_video.filename_root))
+            return mark_safe('<a href="/media/uploads/entice_mode/video_list/intro_video/thumbnails/%s_thumbnail.png" target="_blank"><img src="/media/uploads/entice_mode/video_list/intro_video/thumbnails/%s_thumbnail.png" width="200" /></a>' % (self.intro_video.filename_root, self.intro_video.filename_root))
 
     @property
     def outro_video_thumbnail(self):
         if self.outro_video == '':
             return ''
         else:
-            return mark_safe('<a href="/media/uploads/entice_mode/video_list/outro_video_thumbnails/%s_outro_thumbnail.png" target="_blank"><img src="/media/uploads/entice_mode/video_list/outro_video_thumbnails/%s_outro_thumbnail.png" width="200" /></a>' % (self.outro_video.filename_root, self.outro_video.filename_root))
+            return mark_safe('<a href="/media/uploads/entice_mode/video_list/outro_video/thumbnails/%s_thumbnail.png" target="_blank"><img src="/media/uploads/entice_mode/video_list/outro_video/thumbnails/%s_thumbnail.png" width="200" /></a>' % (self.outro_video.filename_root, self.outro_video.filename_root))
 
     @property
     def touch_indicator_video_thumbnail(self):
         if self.touch_indicator_video == '':
             return ''
         else:
-            return mark_safe('<a href="/media/uploads/entice_mode/video_list/touch_indicator_video_thumbnails/%s_touch_thumbnail.png" target="_blank"><img src="/media/uploads/entice_mode/video_list/touch_indicator_video_thumbnails/%s_touch_thumbnail.png" width="200" /></a>' % (self.touch_indicator_video.filename_root, self.touch_indicator_video.filename_root))
+            return mark_safe('<a href="/media/uploads/entice_mode/video_list/touch_indicator_video/thumbnails/%s_thumbnail.png" target="_blank"><img src="/media/uploads/entice_mode/video_list/touch_indicator_video/thumbnails/%s_thumbnail.png" width="200" /></a>' % (self.touch_indicator_video.filename_root, self.touch_indicator_video.filename_root))
 
     @property
     def background_vignette_video_thumbnail(self):
         if self.background_vignette_video == '':
             return ''
         else:
-            return mark_safe('<a href="/media/uploads/entice_mode/video_list/background_vignette_video_thumbnails/%s_background_thumbnail.png" target="_blank"><img src="/media/uploads/entice_mode/video_list/background_vignette_video_thumbnails/%s_background_thumbnail.png" width="200" /></a>' % (self.background_vignette_video.filename_root, self.background_vignette_video.filename_root))
+            return mark_safe('<a href="/media/uploads/entice_mode/video_list/background_vignette_video/thumbnails/%s_thumbnail.png" target="_blank"><img src="/media/uploads/entice_mode/video_list/background_vignette_video/thumbnails/%s_thumbnail.png" width="200" /></a>' % (self.background_vignette_video.filename_root, self.background_vignette_video.filename_root))
 
 
 @receiver(post_save, sender=Sequence)
@@ -60,9 +60,9 @@ def generate_intro_video_thumbnails(sender, instance, created, **kwargs):
     if created:
         path = BASE_DIR + '/media/' + str(instance.intro_video)
         clip = VideoFileClip(path)
-        thumbnail = str(instance.intro_video.filename_root) + '_intro_thumbnail.png'
+        thumbnail = str(instance.intro_video.filename_root) + '_thumbnail.png'
         clip.save_frame(thumbnail, t=(clip.duration/2))
-        os.rename(BASE_DIR + '/' + str(thumbnail), BASE_DIR + "/media/uploads/entice_mode/video_list/intro_video_thumbnails/" + str(thumbnail))
+        os.rename(BASE_DIR + '/' + str(thumbnail), BASE_DIR + "/media/uploads/entice_mode/video_list/intro_video/thumbnails/" + str(thumbnail))
 
 
 @receiver(post_save, sender=Sequence)
@@ -70,9 +70,9 @@ def generate_outro_video_thumbnails(sender, instance, created, **kwargs):
     if created:
         path = BASE_DIR + '/media/' + str(instance.outro_video)
         clip = VideoFileClip(path)
-        thumbnail = str(instance.outro_video.filename_root) + '_outro_thumbnail.png'
+        thumbnail = str(instance.outro_video.filename_root) + '_thumbnail.png'
         clip.save_frame(thumbnail, t=(clip.duration/2))
-        os.rename(BASE_DIR + '/' + str(thumbnail), BASE_DIR + "/media/uploads/entice_mode/video_list/outro_video_thumbnails/" + str(thumbnail))
+        os.rename(BASE_DIR + '/' + str(thumbnail), BASE_DIR + "/media/uploads/entice_mode/video_list/outro_video/thumbnails/" + str(thumbnail))
 
 
 @receiver(post_save, sender=Sequence)
@@ -80,9 +80,9 @@ def generate_touch_indicator_video_thumbnail(sender, instance, created, **kwargs
     if created:
         path = BASE_DIR + '/media/' + str(instance.touch_indicator_video)
         clip = VideoFileClip(path)
-        thumbnail = str(instance.touch_indicator_video.filename_root) + '_touch_thumbnail.png'
+        thumbnail = str(instance.touch_indicator_video.filename_root) + '_thumbnail.png'
         clip.save_frame(thumbnail, t=(clip.duration/2))
-        os.rename(BASE_DIR + '/' + str(thumbnail), BASE_DIR + "/media/uploads/entice_mode/video_list/touch_indicator_video_thumbnails/" + str(thumbnail))
+        os.rename(BASE_DIR + '/' + str(thumbnail), BASE_DIR + "/media/uploads/entice_mode/video_list/touch_indicator_video/thumbnails/" + str(thumbnail))
 
 
 @receiver(post_save, sender=Sequence)
@@ -90,6 +90,6 @@ def generate_background_vignette_video_thumbnail(sender, instance, created, **kw
     if created:
         path = BASE_DIR + '/media/' + str(instance.background_vignette_video)
         clip = VideoFileClip(path)
-        thumbnail = str(instance.background_vignette_video.filename_root) + '_background_thumbnail.png'
+        thumbnail = str(instance.background_vignette_video.filename_root) + '_thumbnail.png'
         clip.save_frame(thumbnail, t=(clip.duration/2))
-        os.rename(BASE_DIR + '/' + str(thumbnail), BASE_DIR + "/media/uploads/entice_mode/video_list/background_vignette_video_thumbnails/" + str(thumbnail))
+        os.rename(BASE_DIR + '/' + str(thumbnail), BASE_DIR + "/media/uploads/entice_mode/video_list/background_vignette_video/thumbnails/" + str(thumbnail))
