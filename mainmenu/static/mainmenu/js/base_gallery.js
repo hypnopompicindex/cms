c52e198e220d804fda94bc37fad86028ddcbb235
hypnopompicindex <hypnopompicindex@gmail.com>
2018-06-26 01:31:55 -0400

    drag css

--- mainmenu/static/mainmenu/js/base_gallery.js
+++ mainmenu/static/mainmenu/js/base_gallery.js
@@ -3,3 +3,10 @@
         box = $('.field-_reorder').height();
         $(".drag").height(box);});
 })(django.jQuery);
+
+
+(function($) {
+    $(function() {
+        box = $('#result_list').height();
+        $("table").height(box);});
+})(django.jQuery);
