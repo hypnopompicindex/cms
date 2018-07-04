381edda55462cda8d2bc1be160c4d2b332ac5306
hypnopompicindex <hypnopompicindex@gmail.com>
2018-07-04 00:33:06 -0400

    global settings dashboard update

--- dashboard.py
+++ dashboard.py
@@ -9,7 +9,7 @@ class CustomIndexDashboard(Dashboard):
     def init_with_context(self, context):
         self.children.append(modules.AppList(
             _('Applications'),
-            exclude=('auth.*', 'filebrowser.*'),
+            exclude=('auth.*', 'filebrowser.*', 'globalsettings.*'),
             column=0,
             order=0
         )),
@@ -35,5 +35,27 @@ class CustomIndexDashboard(Dashboard):
                 },
             ],
             column=2,
+            order=1
+        ))
+        self.children.append(modules.LinkList(
+            _('Global Settings'),
+            children=[
+                {
+                    'title': 'Content Styling',
+                    'url': '/globalsettings/contentstyling/2/change/',
+                    'external': True,
+                },
+                {
+                    'title': 'Video Wall',
+                    'url': '/globalsettings/videowall/1/change/',
+                    'external': True,
+                },
+                {
+                    'title': 'Publish CMS',
+                    'url': '/globalsettings/publish/1/change/',
+                    'external': True,
+                },
+            ],
+            column=0,
             order=1
         ))
